d9cad85ceb0912f00a87249c5ae802d0b2f0feb2
Saikyuuuu <150668266+Saikyuuuu@users.noreply.github.com>
2024-02-06 14:52:22 +0800

    Add files via upload

---
+++ Water-Billing-System/src/main/java/com/mycompany/water/billing/system/WaterBillingSystem.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.water.billing.system;
+
+
+/**
+ *
+ * @author 63967
+ */
+public class WaterBillingSystem {
+
+    public static void main(String[] args) {
+      Dashboard dashboard = new Dashboard();
+      dashboard.show();
+    }
+}
